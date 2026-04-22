1779e6f2d8d5f967ceddbfd3fecb1422416255e2
Hongyi Lyu <hongyi.lyu95@gmail.com>
2026-04-22 11:46:32 -0400

    init

---
+++ extensions/index.ts
@@ -0,0 +1,6 @@
+import type { ExtensionAPI } from "@mariozechner/pi-coding-agent";
+
+export default function (pi: ExtensionAPI) {
+  // Register your custom provider here
+  // pi.registerProvider("kiro", { ... });
+}
